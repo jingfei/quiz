bffd876509774d68865f13da9933fd3a3f5cd034
jingfei <jingfei955047@gmail.com>
2015-10-19 23:48:30 +0800

    Finish q4 iterative and recursive version

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,25 @@
-/* FIXME: Implement! */
+/* Implement! */
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    return a<b ? b : a;
+}
+
+int maxSubArray(int A[], int n)
+{
+    int last=-1e9, ans=-1e9, i;
+    for(i=0; i<n; ++i) {
+        last = max(0,last)+A[i];
+        ans = max(ans,last);
+    }
+    return ans;
+}
 
 int main()
 {
+    int ex[9]= {-2,1,-3,4,-1,2,1,-5,4};
+    printf("%d\n",maxSubArray(ex,9));
     return 0;
 }
+
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,23 @@
-/* FIXME: Implement! */
+/* Implement! */
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    return a<b ? b : a;
+}
+
+int maxSubArray(int A[], int n, int last, int ans)
+{
+    if(n==0) return ans;
+    last = max(0,last)+A[0];
+    ans = max(ans,last);
+    return maxSubArray(A+1,n-1,last,ans);
+}
 
 int main()
 {
+    int ex[9]= {-2,1,-3,4,-1,2,1,-5,4};
+    printf("%d\n",maxSubArray(ex,9,-1e9,-1e9));
     return 0;
 }
+
